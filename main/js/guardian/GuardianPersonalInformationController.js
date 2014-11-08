87d0aba73fdae1ba72078b4d5f178392baac11cc
Jessica DiMariano <jld3781@users.noreply.github.com>
2014-11-08 15:07:05 -0600

    Update Guardian Personal Info Controller and HTML

--- main/js/guardian/GuardianPersonalInformationController.js
+++ main/js/guardian/GuardianPersonalInformationController.js
@@ -2,6 +2,36 @@ var modules = require("../modules");
 
 modules.controllerModule.controller('GuardianPersonalInformationController', ['$scope', 'ReferenceDataService', 'GuardianPersonalInformationService',
     function ($scope, ReferenceDataService, GuardianPersonalInformationService) {
-
+        $scope.personalInfo = new PersonalInfo();
+        function PersonalInfo(type){
+            this.type = type;
+            this.firstName = "";
+            this.lastName = "";
+            this.middleName = "";
+            this.nickName = "";
+            this.address = "";
+            this.city = "";
+            this.state = "";
+            this.zip = "";
+            this.dayPhone = "";
+            this.eveningPhone = "";
+            this.cellPhone = "";
+            this.email = "";
+            this.occupation = "";
+            this.age = "";
+            this.dateOfBirth = "";
+            this.branch = "";
+            this.whereServed = "";
+            this.whenServed = "";
+            this.veteranName = "";
+        }
+        function validatePersonalInfo(PersonalInfo){
+            //TODO validate Personal Info
+        }
+        $scope.submitForm = function(){
+            if(validatePersonalInfo()){
+                GuardianPersonalInformationService.sendForm($scope.personalInfo);
+            }
+        }
     }
 ]);
