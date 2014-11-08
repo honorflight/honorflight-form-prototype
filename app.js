a1fc490b6587bb74d45707fcec08a3380f318db9
Jeff Ancel <jancel@gmail.com>
2014-11-07 18:40:21 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('honorflightFormPrototype', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('honorflightFormPrototype').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('honorflightFormPrototype').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
