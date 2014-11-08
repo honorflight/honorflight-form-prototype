87872dbfd65c56db1e18ca109d650a6071df5c0f
Gongxia Chen <gxc@gxc-mac.dhcp.wustl.edu>
2014-11-08 14:09:14 -0600

    make veteran comments ng-model binding

--- main/js/veteran/VeteranCommentsController.js
+++ main/js/veteran/VeteranCommentsController.js
@@ -3,5 +3,21 @@ var modules = require("modules");
 modules.controllerModule.controller('VeteranCommentsController', ['$scope', 'ReferenceDataService', 'VeteranCommentsService',
     function ($scope, ReferenceDataService, VeteranCommentsService) {
 
+
+        $scope.contacts = [new Comment("personalReference")];
+        function Comment(){
+            this.comment="";
+        }
+        function validateComment(contact){
+            //TODO validate contact
+        }
+        $scope.submitForm = function(){
+            if(validateComment()){
+
+            }
+        }
+
+
+
     }
 ]);
