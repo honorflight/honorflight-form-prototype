950b7c6e709a3cf1f61bcf4337c7c88690b538cb
SHAFER <christopher.gregory.shafer@monsanto.com>
2014-11-08 12:08:12 -0600

    started work on controller

--- main/js/guardian/GuardianContactsController.js
+++ main/js/guardian/GuardianContactsController.js
@@ -2,6 +2,19 @@ var modules = require("modules");
 
 modules.controllerModule.controller('GuardianContactsController', ['$scope', 'ReferenceDataService', 'GuardianContactsService',
     function ($scope, ReferenceDataService, GuardianContactsService) {
-
+        function contact(type){
+            this.type = type;
+            this.name = "";
+            this.email = "";
+            this.address = "";
+            this.city = "";
+            this.state = "";
+            this.zip = "";
+            this.dayPhone = "";
+            this.evePhone = "";
+        }
+        function validateContact(contact){
+            //TODO validate contact
+        }
     }
 ]);
