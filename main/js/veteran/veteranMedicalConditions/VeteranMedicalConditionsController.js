d4b0ce1dce113847f334d6eb45d1b134de7f614f
Gongxia Chen <gxc@gxc-mac.dhcp.wustl.edu>
2014-11-08 14:59:15 -0600

    update Veteran Medical Conditions

--- main/js/veteran/veteranMedicalConditions/VeteranMedicalConditionsController.js
+++ main/js/veteran/veteranMedicalConditions/VeteranMedicalConditionsController.js
@@ -3,5 +3,25 @@ var modules = require("modules");
 modules.controllerModule.controller('VeteranMedicalConditionsController', ['$scope', 'ReferenceDataService', 'VeteranMedicalConditionsService',
     function ($scope, ReferenceDataService, VeteranMedicalConditionsService) {
 
+        $scope.veteranMedical= new VeteranMedicalConditions()
+        function VeteranMedicalConditions(){
+            this.ifSeizures = false;
+            this.ifGrandMal = false;
+            this.ifPetitMal = false;
+            this.lastSeizure = "";
+            this.breathProblems = "";
+            this.nebulizer = "";
+            this.oxygen = "";
+
+        }
+        function validateVeteranMedicalConditions(contact){
+            //TODO validate contact
+        }
+        $scope.submitForm = function(){
+            if(validateVeteranMedicalConditions()){
+                GuardianContactsService.sendForm($scope.contacts);
+            }
+        }
+
     }
 ]);
